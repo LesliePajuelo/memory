d185eace8c7df745b9657a7a94375d231929ecaf
Leslie Pajuelo <l.l.pajuelo@gmail.com>
2022-10-06 04:19:55 -0700

    Add cpu block

--- app.js
+++ app.js
@@ -2,7 +2,12 @@ require('newrelic');
 const express = require("express");
 const fs = require("fs");
 const app = express();
-const port = 3000;
+const cpu = require("./controller");
+const conf = {
+  CPU_LOAD_PORT: 3000,
+  CPU_LOAD_DEFAULT_PERCENTAGE: 0,
+  CPU_LOAD_DEFAULT_PERCENTAGE_URL: null
+};
 
 const response = fs.readFileSync("./file.txt").toString(); // ~1MB
 
@@ -11,32 +16,68 @@ app.use(express.static('public'));
 app.get("/noop", (req, res)=>{
   res.send("hellow world")
 })
+
+
 app.get("/sendfile", (req, res) => {
   res.send(0 + "\n" + response);
 });
 
-let slowLeak = 0;
+app.get("/sf-clutter", (req, res) => {
+  console.log(req, res)
+  res.send(0 + "\n" + response);
+});
+
+let tiny = 0;
 app.get("/nr-cuz", (req, res) => {
-  slowLeak += 1;
-  res.send(slowLeak + "\n" + response);
+  tiny += 1;
+  res.send(tiny + "\n" + response);
 });
 
-// The array storageSmallLeak has an increasing length, containing references to a long string
-const storageSmallLeak = [];
+const mediumProblem = [];
 app.get("/medium-hold", (req, res) => {
-  storageSmallLeak.push(response);
-  const counterSmallLink = storageSmallLeak.length;
-  res.send(counterSmallLink + "\n" + response);
+  mediumProblem.push(response);
+  res.send(mediumProblem.length + "\n" + response);
 });
 
-// The array storageBigLeak has an increasing length, containing long strings (of 1 MB each)
+//  1 MB Strings
 const storageBigLeak = [];
 app.get("/hold-tight", (req, res) => {
   storageBigLeak.push(fs.readFileSync("./file.txt").toString());
-  const counterBigLink = storageBigLeak.length;
-  res.send(counterBigLink + "\n" + response);
+  res.send(storageBigLeak.length + "\n" + response);
 });
 
-app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
+
+app.get('/blocking', (req, res) => {
+  //git@github.com:jamesskinner/node-cpu-load-server.git
+  
+  let seconds = +req.query.seconds;
+  let percent = +req.query.percent;
+  if (Number.isNaN(percent)) {
+    res.status(400).send('Query param percent must be a number\n');
+    return;
+  }
+  if (req.params.seconds && Number.isNaN(seconds)) {
+    res.status(400).send('Query param seconds must be number\n');
+    return;
+  }
+  if (percent > 100 || percent < 1) {
+    res.status(400).send('Percentage needs to be...a percentage\n');
+    return;
+  }
+  cpu.blockCpu(seconds, percent, conf);
+  res.send(`Blocking ${percent}% for ${seconds} seconds\n`);
+});
+
+
+
+app.listen(conf.CPU_LOAD_PORT, async () => {
+  console.log('App listening',conf.CPU_LOAD_PORT );
+  //git@github.com:jamesskinner/node-cpu-load-server.git
+  await cpu.setDefaultPercentage(conf);
+  cpu.blockCpu(0, conf.CPU_LOAD_DEFAULT_PERCENTAGE, conf);
+  cpu.repeat(async () => {
+    const change = await cpu.setDefaultPercentage(conf);
+    if (!change) return;
+    cpu.blockCpu(1, conf.CPU_LOAD_DEFAULT_PERCENTAGE, conf);
+  }, 5000);
 });
---
+++ controller.js
@@ -0,0 +1,76 @@
+let activeInteval;
+
+
+// Set an interval which blocks for given % of a second, then waits for the
+// remaining part of the second before doing the same again
+function blockCpu(seconds, percent, conf) {
+  clearSingleInterval();
+
+  const blockFor = (1000 / 100) * percent;
+  const waitFor = 1000 - blockFor;
+
+  const handler = singleInterval(() => block(blockFor), waitFor);
+  activeInteval = handler;
+  if (seconds) {
+    setTimeout(() => {
+      if (activeInteval === handler) {
+        blockCpu(1,1,conf);
+      }
+    }, seconds * 1000);
+  }
+}
+
+function block(blockFor) {
+  const blockEnd = Date.now() + blockFor;
+  while (Date.now() < blockEnd) {
+    Math.random(); // eslint-disable-line
+  }
+}
+
+let intervalId;
+function singleInterval(f, timeout, id) {
+  if (!id) {
+    id = Date.now();
+    intervalId = id;
+  }
+  setTimeout(() => {
+    if (id !== intervalId) return;
+    f();
+    singleInterval(f, timeout, id);
+  }, timeout);
+  return id;
+}
+function clearSingleInterval() {
+  intervalId = null;
+}
+
+function filterObj(obj, filter) {
+  return Object.entries(obj)
+    .filter(([k, v]) => filter(v, k))
+    .reduce((acc, [k, v]) => ({ ...acc, [k]: v }), {});
+}
+
+async function setDefaultPercentage(conf) {
+  const before = conf.CPU_LOAD_DEFAULT_PERCENTAGE;
+  if (!conf.CPU_LOAD_DEFAULT_PERCENTAGE_URL) return false;
+  try {
+    const response = await got(conf.CPU_LOAD_DEFAULT_PERCENTAGE_URL);
+    const percent = +response.body;
+    if (Number.isNaN(percent)) throw new Error(`Bad percent from URL: ${response.body}`);
+    conf.CPU_LOAD_DEFAULT_PERCENTAGE = percent;
+  } catch (e) {
+    console.log('Failed to load percent from url', conf.CPU_LOAD_DEFAULT_PERCENTAGE_URL);
+    console.error(e);
+  }
+  if (before !== conf.CPU_LOAD_DEFAULT_PERCENTAGE) return true;
+  return false;
+}
+
+function repeat(task, gap) {
+  task().then(() => {
+    setTimeout(() => repeat(task, gap), gap);
+  });
+}
+
+
+module.exports = {blockCpu, setDefaultPercentage, repeat}
